ac7a4f3b7c74c79ca08fe4fb1c54ea4f3b318335
cbomalick <55862931+cbomalick@users.noreply.github.com>
2019-10-06 18:38:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -93,7 +93,6 @@ ul {
     height: 36px;
     vertical-align: middle;
     text-align: center;
-    text-overflow: ellipsis;
     text-transform: uppercase;
     color: rgb(var(--pure-material-onprimary-rgb, 255, 255, 255));
     background-color: rgb(var(--pure-material-primary-rgb, 33, 150, 243));
@@ -106,68 +105,6 @@ ul {
     outline: none;
     cursor: pointer;
     transition: box-shadow 0.2s;
-}
-
-.button::-moz-focus-inner {
-    border: none;
-}
-
-/* Overlay */
-.button::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background-color: rgb(var(--pure-material-onprimary-rgb, 255, 255, 255));
-    opacity: 0;
-    transition: opacity 0.2s;
-}
-
-/* Ripple */
-.button::after {
-    content: "";
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    border-radius: 50%;
-    padding: 50%;
-    width: 32px; /* Safari */
-    height: 32px; /* Safari */
-    background-color: rgb(var(--pure-material-onprimary-rgb, 255, 255, 255));
-    opacity: 0;
-    transform: translate(-50%, -50%) scale(1);
-    transition: opacity 1s, transform 0.5s;
-}
-
-/* Hover, Focus */
-.button:hover,
-.button:focus {
-    box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 1px 10px 0 rgba(0, 0, 0, 0.12);
-}
-
-.button:hover::before {
-    opacity: 0.08;
-}
-
-.button:focus::before {
-    opacity: 0.24;
-}
-
-.button:hover:focus::before {
-    opacity: 0.3;
-}
-
-/* Active */
-.button:active {
-    box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2), 0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
-}
-
-.button:active::after {
-    opacity: 0.32;
-    transform: translate(-50%, -50%) scale(0);
-    transition: transform 0s;
 }
 
 .button.red {
@@ -273,11 +210,11 @@ Echo'
   <div class="panel">
       <form autocomplete="off" action="?" id="form" method="post" enctype="multipart/form-data">
       <h2>Langelier Saturation Index Calculator</h2>
-      <p>The Langelier Index is an approximate indicator of the calcium carbonate saturation in water.
+      <p>The Langelier Index is an indicator of the calcium carbonate saturation in water.
         <ul>
-          <li>If the index is negative, water is under saturated and will tend to corrode scale</li>
-          <li>If the index is positive, water is over saturated and will tend to deposit scale</li>
-          <li>If the index is close to zero, water will neither strongly corrode or deposit scale</li>
+          <li>When negative, water is under saturated and will tend to corrode scale</li>
+          <li>When positive, water is over saturated and will tend to deposit scale</li>
+          <li>When close to zero, water will neither strongly corrode or deposit scale</li>
         </ul>
       </p>
       <p class="header">pH <span id="phwarning" class="requiredfield"></span></p>
